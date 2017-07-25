94158111a188d17394b97ef26a6d7c11a861df91
Erwin Hoeckx <erwin.hoeckx@cgi.com>
2017-07-25 21:56:47 +0200

    Perfected dry run

--- provision.sh
+++ provision.sh
@@ -7,8 +7,8 @@ sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
 echo "cd /vagrant" >> /home/vagrant/.bashrc
-echo "alias ap='ansible-playbook'" >> /home/vagrant/.bashrc
+echo "alias ap='ansible-playbook -i hosts'" >> /home/vagrant/.bashrc
 chmod 600 /home/vagrant/.ssh/id_rsa
 chmod 600 /home/vagrant/.ssh/id_rsa.pub
 chmod 777 /etc/ansible
-cp /vagrant/hosts /etc/ansible/hosts
+#cp /vagrant/hosts /etc/ansible/hosts
